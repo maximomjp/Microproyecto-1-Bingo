74b9b3bd239d719e98f8500c82456ebc818f859f
Maximo <contactomujicap@gmail.com>
2024-02-11 12:41:50 -0400

    menu-principal1.1

---
+++ script.js
@@ -0,0 +1,6 @@
+const nomJug1 = document.getElementById("jug1")
+const nomJug2 = document.getElementById("jug2")
+const nomJug3 = document.getElementById("jug3")
+const nomJug4 = document.getElementById("jug4")
+const btnEnviar = document.getElementById("boton-enviar")
+
