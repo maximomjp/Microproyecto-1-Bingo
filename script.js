3e3ea3d3e4d39ee61096268c15c3026e69e32119
Maximo <contactomujicap@gmail.com>
2024-02-11 21:04:53 -0400

    javascript-1.0

--- script.js
+++ script.js
@@ -5,29 +5,39 @@ const nomJug3 = document.getElementById("jug3")
 const nomJug4 = document.getElementById("jug4")
 const btnEnviar = document.getElementById("boton-enviar")
 
-//Bingo html
 
-//Creación de cartones
+let matriz = [];
+let filas = 3;
+let col = 3;
+let h = 0;
 
-let matriz3x3 = [[],[],[]];
-
-let matriz4x4 = [[],[],[],[]];
-
-let matriz5x5 = [[],[],[],[],[]];
-
-//Generar numeros aleatorios para cartones
+//Generar numeros aleatorios
 generarAleatorio=(min, max)=>{
     return Math.floor(Math.random() * (max - min)) + min; 
 };
 
-for (let x = 0; x <= 100; x++){
-    console.log(generarAleatorio(1, 51))
+//Relleno de cartones
+for (let i = 0; i < filas; i++) {
+    matriz[i] = [];
+    for (let j = 0; j < col; j++) {
+        matriz[i][j] = h++;
+    }
 }
 
-//Generar numero aletorio para sorteo
-for (let x = 0; x <= 100; x++){
-    console.log(generarAleatorio(1, 51))
+for (let i = 0; i < 25; i++) {
+    matriz.push(generarAleatorio(1,51));
+ }
+
+ //Iterar array
+for(let i = 0; i < matriz.length; i++){
+    console.log(matriz[i])
 }
+
+
+
+
+
+
 
 
 
